959e9878e50c3e23beb0a493a52f8b21da030aac
Seamoon Pandey <hackedasocial@gmail.com>
2024-07-10 09:36:04 +0545

    add sub

--- add_sub.c
+++ add_sub.c
@@ -5,20 +5,26 @@
 
 void decimalToBinary(int n, int *binary, int size)
 {
+    if (n < 0)
+    {
+        n = abs(n);
+    }
+
     for (int i = size - 1; i >= 0; i--)
     {
-        binary[i] = n & 1;
-        n >>= 1;
+        binary[i] = n % 2;
+        n = n / 2;
     }
 }
-
 void twosComplement(int *binary, int size)
 {
     int carry = 1;
+
     for (int i = 0; i < size; i++)
     {
-        binary[i] = ~binary[i] & 1;
+        binary[i] = binary[i] == 0 ? 1 : 0;
     }
+
     for (int i = size - 1; i >= 0; i--)
     {
         binary[i] = binary[i] + carry;
@@ -34,29 +40,43 @@ void twosComplement(int *binary, int size)
     }
 }
 
-void addTwoBinaries(int *binary1, const int *binary2, int size)
+void addTwoBinaries(int *binary1, int *binary2, int size)
 {
     int carry = 0;
     for (int i = size - 1; i >= 0; i--)
     {
-        int sum = binary1[i] ^ binary2[i] ^ carry;
-        carry = (binary1[i] & binary2[i]) | (binary1[i] & carry) | (binary2[i] & carry);
-        binary1[i] = sum;
+        binary1[i] = binary1[i] + binary2[i] + carry;
+        if (binary1[i] == 2)
+        {
+            binary1[i] = 0;
+            carry = 1;
+        }
+        else if (binary1[i] == 3)
+        {
+            binary1[i] = 1;
+            carry = 1;
+        }
+        else
+        {
+            carry = 0;
+        }
     }
 }
 
-void subtractTwoBinaries(int *binary1, const int *binary2, int size)
+void subtractTwoBinaries(int *binary1, int *binary2, int size)
 {
     int twosComp[SIZE];
+
     for (int i = 0; i < size; i++)
     {
         twosComp[i] = binary2[i];
     }
+
     twosComplement(twosComp, size);
     addTwoBinaries(binary1, twosComp, size);
 }
 
-void printBinary(const int *binary, int size)
+void printBinary(int *binary, int size)
 {
     for (int i = 0; i < size; i++)
     {
@@ -67,8 +87,7 @@ void printBinary(const int *binary, int size)
 
 int main()
 {
-    int a1, b1;
-    int a[SIZE] = {0}, b[SIZE] = {0};
+    int a1, b1, a[SIZE], b[SIZE];
 
     printf("Enter the first number: ");
     scanf("%d", &a1);
@@ -94,6 +113,16 @@ int main()
     printf("Second number in binary: ");
     printBinary(b, SIZE);
     printf("\n");
+
+    int sum[SIZE];
+    for (int i = 0; i < SIZE; i++)
+    {
+        sum[i] = a[i];
+    }
+    addTwoBinaries(sum, b, SIZE);
+
+    printf("Sum of the given two numbers: ");
+    printBinary(sum, SIZE);
 
     int diff[SIZE];
     for (int i = 0; i < SIZE; i++)
